25491b92ff9f6f699dcde285061b5d1499610146
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-10-07 17:09:18 +0000

    get sales data for last 5 market days for each sandwhich

--- run.py
+++ run.py
@@ -92,6 +92,28 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+
+
+def get_last_5_enteries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 enteries for each sandwich and returns the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for i in range(1, 7):
+        column = sales.col_values(i)
+        columns.append(column[-5:])
+    pprint(columns)
+
+
+
+get_last_5_enteries_sales()
+
+
+
 def main():
     """
     Run all program functions
@@ -102,5 +124,6 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
 
+
 print("Welcome to love sandwhiches Data Automation")
-main()
+#main()
